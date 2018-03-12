e67f97aa1369ac8fd45588da12351a14d16dca64
Alexis <alexixsortizz@gmail.com>
2018-03-12 09:03:46 -0500

    Se completa la clase MongoRepository agregando los metodos que faltaban

--- src/server/model/VehicleTypeModel.js
+++ src/server/model/VehicleTypeModel.js
@@ -2,13 +2,11 @@
 
 let Entity = require("../model/Entity");
 let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
 
-class VehicleTypeSchema extends Entity {
-    structure() {
-        this.structure();
-        this.def(String, 'description');
-    };
-};
 
-let vehicleTypeSchema = mongoose.Schema(VehicleTypeSchema, { collection: 'vehicle_type' });
-module.exports = mongoose.model('vehicleType', vehicleTypeSchema);
+let VehicleTypeSchema = new Schema({
+    description: String
+}, { collection: 'vehicle_type' });
+
+module.exports = mongoose.model('VehicleTypeModel', VehicleTypeSchema);
--- src/server/repositories/MongoRepository.js
+++ src/server/repositories/MongoRepository.js
@@ -1,24 +1,130 @@
 'use strict';
 
-let AdminModel = require("../model/AdminModel");
-let PriceModel = require("../model/PriceModel");
-
-
 //Mongo client
 let mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/parking");
 
+
+/**
+ * Class to manage DML statements to the mongo db
+ */
 class MongoRepository {
 
-    static findOne(model, query) {
+    /**
+     * Connect to the database
+     */
+    static connect() {
+        mongoose.connect("mongodb://localhost/parking", err => {
+            if (err) {
+                console.log("Couldn't connect to the database");
+            } else {
+                console.log("Connected to the database")
+            }
+        });
+    }
+
+    /**
+     * Method to find records with the given query
+     * @param {Mongoose model} model 
+     * @param {json} query 
+     */
+    static find(model, query) {
         return new Promise((resolve, reject) => {
-            model.find(query).then((doc) => {
-                resolve(doc);
-            }).catch(reject);
+
+            model.find(query, (err, res) => {
+                if (err) {
+                    reject(new Error(err));
+                } else {
+                    resolve(res);
+                }
+            })
         });
     };
+
+    /**
+     * Method to insert one or more documents
+     * @param {Mongoose model} model 
+     * @param {json} documents 
+     */
+    static create(model, documents) {
+        return new Promise((resolve, reject) => {
+            model.create(documents, (err, res) => {
+                if (err) {
+                    reject(new Error(err));
+                } else {
+                    resolve(res);
+                }
+            })
+        })
+    };
+
+    /**
+     * Update the document with the given query
+     * @param {Mongoose model} model 
+     * @param {json} query 
+     * @param {json} document 
+     */
+    static updateOne(model, query, document) {
+        return new Promise((resolve, reject) => {
+            model.update(query, document, (err, res) => {
+                if (err) {
+                    reject(new Error(err));
+                } else {
+                    resolve(res);
+                }
+            })
+        });
+    }
+
+    /**
+     * Update the documents with the given query
+     * @param {Mongoose model} model 
+     * @param {json} query 
+     * @param {json} documents 
+     */
+    static updateMany(model, query, documents) {
+        return new Promise((resolve, reject) => {
+            model.updateMany(query, documents, (err, res) => {
+                if (err) {
+                    reject(new Error(err));
+                } else {
+                    resolve(res);
+                }
+            })
+        })
+    }
+
+    /**
+     * Delete a document with the given query
+     * @param {Mongoose model} model 
+     * @param {json} query 
+     */
+    static deleteOne(model, query) {
+        return new Promise((resolve, reject) => {
+            model.deleteOne(query, (err, res) => {
+                if (err) {
+                    reject(new Error(err));
+                } else {
+                    resolve(res);
+                }
+            })
+        })
+    }
+
+    /**
+     * Delete the documents with the given query
+     * @param {Mongoose model} model 
+     * @param {json} query 
+     */
+    static deleteMany(model, query) {
+        return new Promise((resolve, reject) => {
+            model.deleteMany(query, (err, res) => {
+                if (err) {
+                    reject(new Error(err));
+                } else {
+                    resolve(res);
+                }
+            }).catch(reject);
+        })
+    }
 }
 
-MongoRepository.findOne(PriceModel, { high_engine_price: 2000 }).then(result => {
-    console.log(result);
-})
